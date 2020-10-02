cfb2ca0a9ac8aa352f3f5cc963f18baee1aa60dc
Nabeel <nsahmed@umich.edu>
2020-10-02 03:52:19 -0400

    Update StringExtensions.kt
    Added description to String.upperWithSpace extension

--- src/main/kotlin/me/nabeeeeel/bot/extensions/StringExtensions.kt
+++ src/main/kotlin/me/nabeeeeel/bot/extensions/StringExtensions.kt
@@ -1,5 +1,9 @@
 package me.nabeeeeel.bot.extensions
 
+
+/**
+ * Capitalizes and adds a space to each character in a string: test -> T E S T
+ */
 fun String.upperWithSpace() : String {
     var newString = ""
     forEach {
